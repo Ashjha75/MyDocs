72ca05e7003e2839ddd25aa672896156eb4c7524
Ashish jha <ajha5645@gmail.com>
2025-08-19 13:29:36 +0530

    Deployed 8f22393 with MkDocs version: 1.6.1

--- overrides/extra.js
+++ overrides/extra.js
@@ -1,2 +1,13 @@
 document.getElementsByClassName('md-sidebar--primary')[0].classList.add('pymdownx-highlight');
 document.getElementsByClassName('md-sidebar--secondary')[0].classList.add('pymdownx-highlight');
+
+
+
+
+
+
+
+
+
+
+
