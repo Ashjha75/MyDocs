f018719e8eab77f47e65d26fd6e3f5e799b4f10a
Ashish jha <ajha5645@gmail.com>
2025-08-20 13:57:14 +0530

    Refactor Mermaid.js initialization and update requirements to remove unused plugin

--- docs/overrides/init-mermaid.js
+++ docs/overrides/init-mermaid.js
@@ -1,11 +1,4 @@
-window.addEventListener("load", () => {
-  if (window.mermaid) {
-    console.log("Mermaid version:", mermaid.version);
-    mermaid.initialize({
-      startOnLoad: true,
-      theme: "neo-dark"
-    });
-  } else {
-    console.error("Mermaid not loaded");
-  }
+mermaid.initialize({
+  securityLevel: 'loose',
+  theme: 'base',
 });
