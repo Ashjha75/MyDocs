4bab495d64c8f96ece3e3850ebe50ee25723771f
Ashish jha <ajha5645@gmail.com>
2025-08-20 16:49:36 +0530

    Updates

--- overrides/extra.js
+++
@@ -1,6 +0,0 @@
-if (window.innerWidth > 900) { // Typical breakpoint for desktop/web view
-  const primary = document.getElementsByClassName('md-sidebar--primary')[0];
-  const secondary = document.getElementsByClassName('md-sidebar--secondary')[0];
-  if (primary) primary.classList.add('pymdownx-highlight');
-  if (secondary) secondary.classList.add('pymdownx-highlight');
-}
