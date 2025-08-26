4d90b5dc1a8564b6590debae6b112a8563a811cb
Ashish jha <ajha5645@gmail.com>
2025-08-26 15:06:40 +0530

    feat: add Full Join to MySQL documentation and update navigation links

--- astro.config.mjs
+++ astro.config.mjs
@@ -102,8 +102,9 @@ export default defineConfig({
 								{label: 'Join Basics', link: '/mysql/join/joinbasics'},
 								{label: 'Inner Join', link: '/mysql/join/inner-join'},
 								{label: 'Left-Right Join', link: '/mysql/join/left-right-join'},
+								{label: 'Full Join', link: '/mysql/join/full-join'},
 								{label: 'Cross Join', link: '/mysql/join/cross-join'},
-								{label: 'Union Join', link: '/mysql/join/union-union-all-join'}
+								{label: 'Union Join', link: '/mysql/join/union-union-all'}
 							]
 						}
 
