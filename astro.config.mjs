5234946544534d92ce086a71916570d706abd813
Ashish jha <ajha5645@gmail.com>
2025-08-29 22:55:31 +0530

    feat: update sidebar structure in Astro configuration for improved Spring Boot and MySQL documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,128 +40,128 @@ export default defineConfig({
                 attrs: { src: '/MyDocs/scripts/zoom.js', defer: true }, // <-- your custom script
             },],
         sidebar: [
-            // {
-            //     label: '🌱 Springboot',
-            //     items: [
-            //         {
-            //             label: 'Base',
-            //             items: [
-            //                 { label: 'Rest Basics', link: '/springboot/base/rest' },
-            //                 { label: 'Attacks', link: '/springboot/base/common-attacks' },
+            {
+                label: '🌱 Springboot',
+                items: [
+                    {
+                        label: 'Base',
+                        items: [
+                            { label: 'Rest Basics', link: '/springboot/base/rest' },
+                            { label: 'Attacks', link: '/springboot/base/common-attacks' },
 
 
-            //             ],
-            //         },
-            //         {
-            //             label: 'Core',
-            //             items: [
-            //                 { label: 'Springboot starter', link: '/springboot/core/springboot-starter' },
-            //                 { label: 'Springboot  MVC Request Flow', link: '/springboot/core/core2' },
-            //                 { label: 'Springboot Internals', link: '/springboot/core/core3' },
-            //                 { label: 'Springboot Validation & DTOs', link: '/springboot/core/core4' },
-            //                 { label: 'Springboot Exception Handling', link: '/springboot/core/exception-handling' },
-            //                 { label: 'Building REST Endpoints', link: '/springboot/core/rest-api' },
-            //                 { label: 'Springboot AOP', link: '/springboot/core/aop' },
-            //                 { label: 'Essentials Spring Boot Compendium', link: '/springboot/core/essentials' },
+                        ],
+                    },
+                    {
+                        label: 'Core',
+                        items: [
+                            { label: 'Springboot starter', link: '/springboot/core/springboot-starter' },
+                            { label: 'Springboot  MVC Request Flow', link: '/springboot/core/core2' },
+                            { label: 'Springboot Internals', link: '/springboot/core/core3' },
+                            { label: 'Springboot Validation & DTOs', link: '/springboot/core/core4' },
+                            { label: 'Springboot Exception Handling', link: '/springboot/core/exception-handling' },
+                            { label: 'Building REST Endpoints', link: '/springboot/core/rest-api' },
+                            { label: 'Springboot AOP', link: '/springboot/core/aop' },
+                            { label: 'Essentials Spring Boot Compendium', link: '/springboot/core/essentials' },
 
-            //             ],
-            //         },
-            //         {
-            //             label: 'MVC',
-            //             items: [
-            //                 { label: 'Spring Mvc 1', link: '/springboot/mvc/mvc1' },
-            //                 { label: 'Spring Mvc 2', link: '/springboot/mvc/mvc2' },
-            //             ],
-            //         },
-            //         {
-            //             label: 'Spring Data Jpa',
-            //             items: [
-            //                 { label: 'Jpa Core', link: '/springboot/jpa/jpacore' },
-            //                 { label: 'Jpa Caching', link: '/springboot/jpa/jpacaching' },
-            //                 { label: 'Modeling & Repositories', link: '/springboot/jpa/models-repo' },
-            //                 { label: 'Relational Mappings', link: '/springboot/jpa/relational-mappings' },
-            //                 { label: 'Querying Techniques', link: '/springboot/jpa/query' },
-            //                 { label: 'Transaction Management', link: '/springboot/jpa/transaction' },
-            //                 { label: 'Auditing & Lifecycle Events', link: '/springboot/jpa/auditing' },
-            //                 { label: 'Performance & Concurrency', link: '/springboot/jpa/advance' },
-            //             ],
-            //         },
-            //         {
-            //             label: 'Spring Security',
-            //             items: [
-            //                 { label: 'Spring Security Basics', link: '/springboot/security/security-base' },
-            //                 { label: 'Spring Security User Creations', link: '/springboot/security/users-creations' },
-            //                 { label: 'Spring Security Web Securities', link: '/springboot/security/web-securities' },
-            //                 { label: 'Spring Security JWT', link: '/springboot/security/jwt' },
-            //                 // { label: 'Spring Security User Creations', link: '/springboot/security/users-creations' },
-            //             ],
-            //         },
-            //     ],
-            // },
+                        ],
+                    },
+                    {
+                        label: 'MVC',
+                        items: [
+                            { label: 'Spring Mvc 1', link: '/springboot/mvc/mvc1' },
+                            { label: 'Spring Mvc 2', link: '/springboot/mvc/mvc2' },
+                        ],
+                    },
+                    {
+                        label: 'Spring Data Jpa',
+                        items: [
+                            { label: 'Jpa Core', link: '/springboot/jpa/jpacore' },
+                            { label: 'Jpa Caching', link: '/springboot/jpa/jpacaching' },
+                            { label: 'Modeling & Repositories', link: '/springboot/jpa/models-repo' },
+                            { label: 'Relational Mappings', link: '/springboot/jpa/relational-mappings' },
+                            { label: 'Querying Techniques', link: '/springboot/jpa/query' },
+                            { label: 'Transaction Management', link: '/springboot/jpa/transaction' },
+                            { label: 'Auditing & Lifecycle Events', link: '/springboot/jpa/auditing' },
+                            { label: 'Performance & Concurrency', link: '/springboot/jpa/advance' },
+                        ],
+                    },
+                    {
+                        label: 'Spring Security',
+                        items: [
+                            { label: 'Spring Security Basics', link: '/springboot/security/security-base' },
+                            { label: 'Spring Security User Creations', link: '/springboot/security/users-creations' },
+                            { label: 'Spring Security Web Securities', link: '/springboot/security/web-securities' },
+                            { label: 'Spring Security JWT', link: '/springboot/security/jwt' },
+                            // { label: 'Spring Security User Creations', link: '/springboot/security/users-creations' },
+                        ],
+                    },
+                ],
+            },
 
-            // {
-            //     label: '⛈️ Mysql',
-            //     items: [
-            //         { label: 'Basics', link: '/mysql/mysqlbasic' },
-            //         { label: 'DDL', link: '/mysql/mysqlddl' },
+            {
+                label: '⛈️ Mysql',
+                items: [
+                    { label: 'Basics', link: '/mysql/mysqlbasic' },
+                    { label: 'DDL', link: '/mysql/mysqlddl' },
 
-            //         {
-            //             label: "Retrival",
-            //             items: [
-            //                 { label: 'Comparision Operator', link: '/mysql/retrival/comparision-operator' },
-            //                 { label: 'Logical Operator', link: '/mysql/retrival/logical-operator' },
-            //                 { label: 'Range Set Operators', link: '/mysql/retrival/range-setoperators' },
-            //                 { label: 'Patterns Matching', link: '/mysql/retrival/patterns-matching' },
-            //                 { label: 'Distinct', link: '/mysql/retrival/distinct' },
-            //                 { label: 'Aliases', link: '/mysql/retrival/aliases' },
-            //                 { label: 'Order By', link: '/mysql/retrival/order-by' },
-            //                 { label: 'Limits', link: '/mysql/retrival/limits' },
-            //                 { label: 'Select', link: '/mysql/retrival/select' },
-            //             ]
-            //         },
-            //         { label: 'Functions', link: '/mysql/function' },
-            //         { label: 'Group By', link: '/mysql/groupby' },
-            //         { label: 'Keys', link: '/mysql/keys' },
-            //         { label: 'Normalization', link: '/mysql/normalization' },
-            //         {
-            //             label: "Joins",
-            //             items: [
-            //                 { label: 'Join Basics', link: '/mysql/join/joinbasics' },
-            //                 { label: 'Inner Join', link: '/mysql/join/inner-join' },
-            //                 { label: 'Left-Right Join', link: '/mysql/join/left-right-join' },
-            //                 { label: 'Union Join', link: '/mysql/join/union-union-all' },
-            //                 { label: 'Full Join', link: '/mysql/join/full-join' },
-            //                 { label: 'Cross Join', link: '/mysql/join/cross-join' },
-            //             ]
-            //         },
-            //         {
-            //             label: "Subqueries", link: '/mysql/subquery'
-            //         }
-            //         ,
-            //         {
-            //             label: "Update", link: '/mysql/update'
-            //         }
-            //         ,
-            //         {
-            //             label: "Delete", link: '/mysql/delete'
-            //         }
-            //         ,
-            //         {
-            //             label: "Replace", link: '/mysql/replace'
-            //         }
+                    {
+                        label: "Retrival",
+                        items: [
+                            { label: 'Comparision Operator', link: '/mysql/retrival/comparision-operator' },
+                            { label: 'Logical Operator', link: '/mysql/retrival/logical-operator' },
+                            { label: 'Range Set Operators', link: '/mysql/retrival/range-setoperators' },
+                            { label: 'Patterns Matching', link: '/mysql/retrival/patterns-matching' },
+                            { label: 'Distinct', link: '/mysql/retrival/distinct' },
+                            { label: 'Aliases', link: '/mysql/retrival/aliases' },
+                            { label: 'Order By', link: '/mysql/retrival/order-by' },
+                            { label: 'Limits', link: '/mysql/retrival/limits' },
+                            { label: 'Select', link: '/mysql/retrival/select' },
+                        ]
+                    },
+                    { label: 'Functions', link: '/mysql/function' },
+                    { label: 'Group By', link: '/mysql/groupby' },
+                    { label: 'Keys', link: '/mysql/keys' },
+                    { label: 'Normalization', link: '/mysql/normalization' },
+                    {
+                        label: "Joins",
+                        items: [
+                            { label: 'Join Basics', link: '/mysql/join/joinbasics' },
+                            { label: 'Inner Join', link: '/mysql/join/inner-join' },
+                            { label: 'Left-Right Join', link: '/mysql/join/left-right-join' },
+                            { label: 'Union Join', link: '/mysql/join/union-union-all' },
+                            { label: 'Full Join', link: '/mysql/join/full-join' },
+                            { label: 'Cross Join', link: '/mysql/join/cross-join' },
+                        ]
+                    },
+                    {
+                        label: "Subqueries", link: '/mysql/subquery'
+                    }
+                    ,
+                    {
+                        label: "Update", link: '/mysql/update'
+                    }
+                    ,
+                    {
+                        label: "Delete", link: '/mysql/delete'
+                    }
+                    ,
+                    {
+                        label: "Replace", link: '/mysql/replace'
+                    }
 
-            //     ],
-            // },
+                ],
+            },
             {
                 label: '🤖 Context',
                 autogenerate: { directory: 'context' },
             },
-            // {
-            //     label: '🪸 Devops',
-            //     items: [
-            //         { label: 'Basics', link: '/devops/docker/basics' }
-            //     ]
-            // },
+            {
+                label: '🪸 Devops',
+                items: [
+                    { label: 'Basics', link: '/devops/docker/basics' }
+                ]
+            },
         ],
 		})],
 });
