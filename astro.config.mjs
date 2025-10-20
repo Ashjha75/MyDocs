8bdba980c062eaa56f5bc9a568db9694a81f90e8
Ashish jha <ajha5645@gmail.com>
2025-10-20 15:09:25 +0530

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import rehypeMermaid from 'rehype-mermaid';
 import starlightThemeGalaxy from 'starlight-theme-galaxy'
+
 // https://astro.build/config
 export default defineConfig({
     markdown: {
